3018ed3624533196430ac4ddc317931870eaf7f5
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-04-15 01:06:59 -0300

    feat(app): create folder alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+	alias: {
+		'@': './src',
+		'@assets': './src/assets',
+		'@screens': './src/screens',
+		'@utils': './src/utils',
+		'@components': './src/components',
+		'@routes': './src/routes',
+		'@theme': './src/theme',
+		'@store': './src/store',
+		'@services': './src/services',
+		'@hooks': './src/hooks'
+	},
 };
