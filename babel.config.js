c12a2f2971ca5d782d3b766bb0884412d9dc5220
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-04-15 23:48:53 -0300

    feat: set custom fonts

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-	alias: {
-		'@': './src',
-		'@assets': './src/assets',
-		'@screens': './src/screens',
-		'@utils': './src/utils',
-		'@components': './src/components',
-		'@routes': './src/routes',
-		'@theme': './src/theme',
-		'@store': './src/store',
-		'@services': './src/services',
-		'@hooks': './src/hooks'
-	},
+  plugins: [
+    [
+			require.resolve('babel-plugin-module-resolver'),
+			{
+				root: ["./src/"],
+				alias: {
+					'@': './src',
+					'@assets': './src/assets',
+					'@screens': './src/screens',
+					'@utils': './src/utils',
+					'@components': './src/components',
+					'@routes': './src/routes',
+					'@theme': './src/theme',
+					'@store': './src/store',
+					'@services': './src/services',
+					'@hooks': './src/hooks'
+				},
+    	}
+		],
+  ],
 };
