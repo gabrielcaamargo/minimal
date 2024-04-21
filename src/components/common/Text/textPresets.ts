1d2ef07524ee06009ce650abbdacd1496880d9c3
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-04-21 18:58:11 -0300

    feat: create text component presets

---
+++ src/components/common/Text/textPresets.ts
@@ -0,0 +1,16 @@
+import { TPresets } from './types';
+
+export const textPresets: TPresets = {
+	default: {
+		color: 'zincLightest',
+	},
+	alert: {
+		color: 'yellowDark',
+	},
+	danger: {
+		color: 'redDark',
+	},
+	success: {
+		color: 'emeraldDark',
+	},
+};
